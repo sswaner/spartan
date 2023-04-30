0846386ba9a4ff79f9c5b99b8daef87e344c9c91
Shawn Swaner <sswaner@athene.com>
2023-04-30 01:13:31 -0500

    first integration with Auth0

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-import boto3
 import json
-from flask import Flask, render_template
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+    
+print(env.get("AUTH0_DOMAIN"))
+app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+
 
 app = Flask(__name__)
 
@@ -29,5 +57,16 @@ def product1():
 def product2():
     return render_template('product2.html')
 
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0', threaded=True)
+    app.run(port=8000, host='0.0.0.0', threaded=True)
